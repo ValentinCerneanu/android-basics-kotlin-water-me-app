addf76c37e11838856f76ffa0b8330b7e6675a53
ValentinCerneanu <valentincerneanu28@gmail.com>
2021-12-16 14:24:52 +0200

    Resolve TODOs

--- app/src/main/java/com/example/waterme/viewmodel/PlantViewModel.kt
+++ app/src/main/java/com/example/waterme/viewmodel/PlantViewModel.kt
@@ -18,11 +18,12 @@ package com.example.waterme.viewmodel
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.work.WorkManager
+import androidx.work.*
 import com.example.waterme.data.DataSource
+import com.example.waterme.worker.WaterReminderWorker
 import java.util.concurrent.TimeUnit
 
-class PlantViewModel(application: Application): ViewModel() {
+class PlantViewModel(val application: Application): ViewModel() {
 
     val plants = DataSource.plants
 
@@ -32,11 +33,19 @@ class PlantViewModel(application: Application): ViewModel() {
         plantName: String
     ) {
         // TODO: create a Data instance with the plantName passed to it
+        val dataBuilder = Data.Builder()
+        val data = dataBuilder.put(WaterReminderWorker.nameKey, plantName).build()
 
         // TODO: Generate a OneTimeWorkRequest with the passed in duration, time unit, and data
         //  instance
+        val oneTimeWorkRequestBuilder = OneTimeWorkRequestBuilder<WaterReminderWorker>()
+        val oneTimeWorkRequest = oneTimeWorkRequestBuilder.setInputData(data).build()
 
         // TODO: Enqueue the request as a unique work request
+        val workManager = WorkManager.getInstance(application)
+        workManager.enqueueUniqueWork(plantName,
+            ExistingWorkPolicy.REPLACE,
+            oneTimeWorkRequest)
     }
 }
 
